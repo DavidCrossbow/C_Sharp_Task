a73096e77e1f44641f4c18e606c092a2e3673e40
Gleb Suslo <suslo.gg@yahoo.com>
2020-04-16 14:16:37 +0300

    Even_symbols project added

---
+++ Even_symbols/Even_symbols/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Even_symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВЫВОД ЧЁТНЫХ СИМВОЛОВ ИЗ СТРОКИ");
+            Console.WriteLine("Введите строку и нажмите Enter");
+            String str = Console.ReadLine();
+            char[] x = str.ToCharArray(); // преобразование строки в массив, первый символ строки стал нулевым элементом массива
+
+            for (int i = 1; i < x.Length; i = i + 2) //  элементы массива выбираются через один, начиная с первого элемента (второй символ в строке) 
+            {
+                Console.Write($"{x[i]}"); // вывод элементов строкой
+            }
+            Console.WriteLine();
+        }
+    }
+}
